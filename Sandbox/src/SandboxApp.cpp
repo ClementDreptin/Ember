b9444926614b3d3fe2842f18c8d19f07d51b5d34
Clément Dreptin <cdreptin@gmail.com>
2020-07-31 16:16:02 +0200

    Added Multi-Threaded build option for all configurations

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,9 +1,22 @@
 #include <GameEngine.h>
 
+class ExampleLayer : public GameEngine::Layer {
+public:
+	ExampleLayer() : Layer("Example") {}
+
+	void onUpdate() override {
+		GE_INFO("ExampleLayer::onUpdate");
+	}
+
+	void onEvent(GameEngine::Event& event) override {
+		GE_TRACE("{0}", event);
+	}
+};
+
 class Sandbox : public GameEngine::App {
 public:
 	Sandbox() {
-
+		pushLayer(new ExampleLayer());
 	}
 
 	~Sandbox() {
--- premake5.lua
+++ premake5.lua
@@ -46,14 +46,17 @@ project "GameEngine"
 
 	filter "configurations:Debug"
 		defines "GE_DEBUG"
+		buildoptions "/MDd"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "GE_RELEASE"
+		buildoptions "/MD"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "GE_DIST"
+		buildoptions "/MD"
 		optimize "On"
 
 
@@ -80,12 +83,15 @@ project "Sandbox"
 
 	filter "configurations:Debug"
 		defines "GE_DEBUG"
+		buildoptions "/MDd"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "GE_RELEASE"
+		buildoptions "/MD"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "GE_DIST"
+		buildoptions "/MD"
 		optimize "On"
