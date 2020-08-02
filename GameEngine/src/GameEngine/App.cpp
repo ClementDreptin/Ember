a85a39d0792f8dece511b6a5cb6551ca58d2cc95
Clément Dreptin <cdreptin@gmail.com>
2020-08-02 06:28:17 +0200

    Added BIND_EVENT_FN macro to the global scope and made an App instance available everywhere

--- GameEngine/src/GameEngine/App.cpp
+++ GameEngine/src/GameEngine/App.cpp
@@ -6,11 +6,14 @@
 #include <glad/glad.h>
 
 namespace GameEngine {
-	#define BIND_EVENT_FN(x) std::bind(&App::x, this, std::placeholders::_1)
+	App* App::s_Instance = nullptr;
 
 	App::App() {
+		GE_CORE_ASSERT(!s_Instance, "App already exists!");
+		s_Instance = this;
+
 		m_Window = std::unique_ptr<Window>(Window::create());
-		m_Window->setEventCallback(BIND_EVENT_FN(onEvent));
+		m_Window->setEventCallback(GE_BIND_EVENT_FN(App::onEvent));
 	}
 
 	App::~App() {
@@ -19,15 +22,17 @@ namespace GameEngine {
 
 	void App::pushLayer(Layer* layer) {
 		m_LayerStack.pushLayer(layer);
+		layer->onAttach();
 	}
 
-	void App::pushOverlay(Layer* overlay) {
-		m_LayerStack.pushOverlay(overlay);
+	void App::pushOverlay(Layer* layer) {
+		m_LayerStack.pushOverlay(layer);
+		layer->onAttach();
 	}
 
 	void App::onEvent(Event& e) {
 		EventDispatcher dispatcher(e);
-		dispatcher.dispatch<WindowCloseEvent>(BIND_EVENT_FN(onWindowClose));
+		dispatcher.dispatch<WindowCloseEvent>(GE_BIND_EVENT_FN(App::onWindowClose));
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();) {
 			(*--it)->onEvent(e);
