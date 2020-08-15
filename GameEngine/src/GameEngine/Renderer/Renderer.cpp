a3a616deec76c7b81ed1ed32249ada4a5802dbe1
Clément Dreptin <cdreptin@gmail.com>
2020-08-15 06:23:50 +0200

    Added a transform argument to the submit method to be able to place elements somewhere in the world

--- GameEngine/src/GameEngine/Renderer/Renderer.cpp
+++ GameEngine/src/GameEngine/Renderer/Renderer.cpp
@@ -10,9 +10,10 @@ namespace GameEngine {
 
 	void Renderer::endScene() {}
 
-	void Renderer::submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray> vertexArray) {
+	void Renderer::submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray> vertexArray, const glm::mat4& transform) {
 		shader->bind();
 		shader->uploadUniformMat4("u_ViewProjection", m_SceneData->viewProjectionMatrix);
+		shader->uploadUniformMat4("u_Transform", transform);
 
 		vertexArray->bind();
 		RenderCommand::drawIndexed(vertexArray);
