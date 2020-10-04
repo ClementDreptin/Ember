4df9ab57e359594e0c4356e3bb55991113bddc9c
Clément Dreptin <cdreptin@gmail.com>
2020-10-04 07:14:46 +0200

    Created a map of tiles with a string

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -4,9 +4,29 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f) {
-	
-}
+// s_MapTiles is a string reprensenting the map
+// W: water
+// D: dirt
+
+static uint32_t s_MapWidth = 24;
+static const char* s_MapTiles = 
+	"WWWWWWWWWWWWWWWWWWWWWWWW"
+	"WWWWWWWDDDDDDWWWWWWWCWWW"
+	"WWWWWDDDDDDDDDDDWWWWWWWW"
+	"WWWWDDDDDDDDDDDDDDDWWWWW"
+	"WWWDDDDDDDDDDDDDDDDDDWWW"
+	"WWDDDDWWWDDDDDDDDDDDDWWW"
+	"WDDDDDWWWDDDDDDDDDDDDDWW"
+	"WWDDDDDDDDDDDDDDDDDDDWWW"
+	"WWWWDDDDDDDDDDDDDDDDWWWW"
+	"WWWWWDDDDDDDDDDDDDDWWWWW"
+	"WWWWWWDDDDDDDDDDDWWWWWWW"
+	"WWWWWWWDDDDDDDDDWWWWWWWW"
+	"WWWWWWWWWWDDDDWWWWWWWWWW"
+	"WWWWWWWWWWWWWWWWWWWWWWWW"
+;
+
+Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f) {}
 
 void Sandbox2D::OnAttach() {
 	EB_PROFILE_FUNCTION();
@@ -15,8 +35,13 @@ void Sandbox2D::OnAttach() {
 	m_SpriteSheet = Ember::Texture2D::Create("assets/game/textures/RPGpack_sheet_2X.png");
 
 	m_TextureStairs = Ember::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 7, 6 }, { 128, 128 });
-	m_TextureBarrel = Ember::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 8, 2 }, { 128, 128 });
 	m_TextureTree = Ember::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 2, 1 }, { 128, 128 }, { 1, 2 });
+
+	m_MapWidth = s_MapWidth;
+	m_MapHeight = strlen(s_MapTiles) / s_MapWidth;
+
+	s_TextureMap['W'] = Ember::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 11, 11 }, { 128, 128 });
+	s_TextureMap['D'] = Ember::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 6, 11 }, { 128, 128 });
 
 	m_Particle.ColorBegin = { 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
 	m_Particle.ColorEnd = { 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 1.0f };
@@ -25,6 +50,8 @@ void Sandbox2D::OnAttach() {
 	m_Particle.Velocity = { 0.0f, 0.0f };
 	m_Particle.VelocityVariation = { 3.0f, 1.0f };
 	m_Particle.Position = { 0.0f, 0.0f };
+
+	m_CameraController.SetZoomLevel(5.0f);
 }
 
 void Sandbox2D::OnDetach() {
@@ -86,9 +113,26 @@ void Sandbox2D::OnUpdate(Ember::Timestep ts) {
 	m_ParticleSystem.OnRender(m_CameraController.GetCamera());
 
 	Ember::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Ember::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.5f }, { 1.0f, 1.0f }, m_TextureStairs);
-	Ember::Renderer2D::DrawQuad({ 1.0f, 0.0f, 0.5f }, { 1.0f, 1.0f }, m_TextureBarrel);
-	Ember::Renderer2D::DrawQuad({ -1.0f, 0.0f, 0.5f }, { 1.0f, 2.0f }, m_TextureTree);
+
+	for (uint32_t y = 0; y < m_MapHeight; y++) {
+		for (uint32_t x = 0; x < m_MapWidth; x++) {
+			char tileType = s_MapTiles[x + y * m_MapWidth];
+			Ember::Ref<Ember::SubTexture2D> texture;
+
+			if (s_TextureMap.find(tileType) != s_TextureMap.end()) {
+				texture = s_TextureMap[tileType];
+			} else {
+				texture = m_TextureStairs;
+			}
+
+			Ember::Renderer2D::DrawQuad({ x - m_MapWidth / 2.0f, y - m_MapHeight / 2.0f, 0.5f }, { 1.0f, 1.0f }, texture);
+		}
+	}
+
+
+	//Ember::Renderer2D::DrawQuad({ 1.0f, 0.0f, 0.5f }, { 1.0f, 1.0f }, m_TextureBarrel);
+	//Ember::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.5f }, { 1.0f, 1.0f }, m_TextureStairs);
+	//Ember::Renderer2D::DrawQuad({ -1.0f, 0.0f, 0.5f }, { 1.0f, 2.0f }, m_TextureTree);
 	Ember::Renderer2D::EndScene();
 }
 
