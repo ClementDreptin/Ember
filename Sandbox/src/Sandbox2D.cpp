e51624b96d4e003f836e73c9d601cc3710afb247
Clément Dreptin <cdreptin@gmail.com>
2020-10-08 12:11:20 +0200

    Removed the dockspace from Sandbox and made it like it was before

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -13,11 +13,6 @@ void Sandbox2D::OnAttach() {
 	EB_PROFILE_FUNCTION();
 
 	m_CheckerboardTexture = Ember::Texture2D::Create("assets/textures/Checkerboard.png");
-
-	Ember::FrameBufferSpec spec;
-	spec.Width = 1280;
-	spec.Height = 720;
-	m_FrameBuffer = Ember::FrameBuffer::Create(spec);
 }
 
 void Sandbox2D::OnDetach() {
@@ -33,7 +28,6 @@ void Sandbox2D::OnUpdate(Ember::Timestep ts) {
 
 	{
 		EB_PROFILE_SCOPE("Renderer Prep");
-		m_FrameBuffer->Bind();
 		Ember::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Ember::RenderCommand::Clear();
 	}
@@ -58,91 +52,24 @@ void Sandbox2D::OnUpdate(Ember::Timestep ts) {
 			}
 		}
 		Ember::Renderer2D::EndScene();
-		m_FrameBuffer->Unbind();
 	}
 }
 
 void Sandbox2D::OnImGuiRender() {
 	EB_PROFILE_FUNCTION();
 
-	// Note: Switch this to true to enable dockspace
-	static bool dockingEnabled = true;
-	if (dockingEnabled) {
-		static bool dockspaceOpen = true;
-		static bool opt_fullscreen_persistant = true;
-		bool opt_fullscreen = opt_fullscreen_persistant;
-		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+	ImGui::Begin("Settings");
 
-		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-		if (opt_fullscreen) {
-			ImGuiViewport* viewport = ImGui::GetMainViewport();
-			ImGui::SetNextWindowPos(viewport->Pos);
-			ImGui::SetNextWindowSize(viewport->Size);
-			ImGui::SetNextWindowViewport(viewport->ID);
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-			window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-			window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-		}
+	auto stats = Ember::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
 
-		if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-			window_flags |= ImGuiWindowFlags_NoBackground;
+	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-		ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
-		ImGui::PopStyleVar();
-
-		if (opt_fullscreen)
-			ImGui::PopStyleVar(2);
-
-		// DockSpace
-		ImGuiIO& io = ImGui::GetIO();
-		if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
-			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-		}
-
-		if (ImGui::BeginMenuBar()) {
-			if (ImGui::BeginMenu("File")) {
-				if (ImGui::MenuItem("Exit")) Ember::App::Get().Close();
-				ImGui::EndMenu();
-			}
-
-			ImGui::EndMenuBar();
-		}
-
-		ImGui::Begin("Settings");
-
-		auto stats = Ember::Renderer2D::GetStats();
-		ImGui::Text("Renderer2D Stats:");
-		ImGui::Text("Draw Calls: %d", stats.DrawCalls);
-		ImGui::Text("Quads: %d", stats.QuadCount);
-		ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
-		ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
-
-		ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
-
-		uint32_t textureID = m_FrameBuffer->GetColorAttachmentRendererID();
-		ImGui::Image((void*)textureID, ImVec2{ 1280.0f, 720.0f });
-		ImGui::End();
-
-		ImGui::End();
-	} else {
-		ImGui::Begin("Settings");
-
-		auto stats = Ember::Renderer2D::GetStats();
-		ImGui::Text("Renderer2D Stats:");
-		ImGui::Text("Draw Calls: %d", stats.DrawCalls);
-		ImGui::Text("Quads: %d", stats.QuadCount);
-		ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
-		ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
-
-		ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
-
-		uint32_t textureID = m_CheckerboardTexture->GetRendererID();
-		ImGui::Image((void*)textureID, ImVec2{ 256.0f, 256.0f });
-		ImGui::End();
-	}
+	ImGui::End();
 }
 
 void Sandbox2D::OnEvent(Ember::Event& e) {
