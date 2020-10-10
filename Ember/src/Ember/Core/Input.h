c749ccec6d65c996ab1195f51d970ed59d4fc588
Clément Dreptin <cdreptin@gmail.com>
2020-10-10 03:05:33 +0200

    Refactored the input class to be more simple

--- Ember/src/Ember/Core/Input.h
+++ Ember/src/Ember/Core/Input.h
@@ -5,20 +5,11 @@
 namespace Ember {
 	class Input {
 	public:
-		inline static bool IsKeyPressed(int keyCode) { return s_Instance->IsKeyPressedImpl(keyCode); }
+		static bool IsKeyPressed(int keyCode);
 
-		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-	protected:
-		virtual bool IsKeyPressedImpl(int KeyCode) = 0;
-
-		virtual bool IsMouseButtonPressedImpl(int button) = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-	private:
-		static Input* s_Instance;
+		static bool IsMouseButtonPressed(int button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 }
--- Ember/src/Platform/Windows/WindowsInput.cpp
+++ Ember/src/Platform/Windows/WindowsInput.cpp
@@ -1,42 +1,41 @@
 #include "ebpch.h"
-#include "WindowsInput.h"
+#include "Ember/Core/Input.h"
 
 #include "Ember/Core/App.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Ember {
-	Input* Input::s_Instance = new WindowsInput();
 
-	bool WindowsInput::IsKeyPressedImpl(int keyCode) {
+	bool Input::IsKeyPressed(int keyCode) {
 		auto window = static_cast<GLFWwindow*>(App::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, keyCode);
 
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(int button) {
+	bool Input::IsMouseButtonPressed(int button) {
 		auto window = static_cast<GLFWwindow*>(App::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, button);
 
 		return state == GLFW_PRESS;
 	}
 
-	float WindowsInput::GetMouseXImpl() {
-		auto [x, y] = GetMousePositionImpl();
-		return x;
-	}
-
-	float WindowsInput::GetMouseYImpl() {
-		auto [x, y] = GetMousePositionImpl();
-		return y;
-	}
-
-	std::pair<float, float> WindowsInput::GetMousePositionImpl() {
+	std::pair<float, float> Input::GetMousePosition() {
 		auto window = static_cast<GLFWwindow*>(App::Get().GetWindow().GetNativeWindow());
 		double xPos, yPos;
 		glfwGetCursorPos(window, &xPos, &yPos);
 
 		return { (float)xPos, (float)yPos };
+	}
+
+	float Input::GetMouseX() {
+		auto [x, y] = GetMousePosition();
+		return x;
+	}
+
+	float Input::GetMouseY() {
+		auto [x, y] = GetMousePosition();
+		return y;
 	}
 }
