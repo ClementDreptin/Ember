ae0e616ac41130faf052391ca463bc9d9707ef3f
Clement <cdreptin@gmail.com>
2020-12-14 06:13:58 +0100

    Removed spdlog related warnings

--- Ember/src/Ember/Core/Log.h
+++ Ember/src/Ember/Core/Log.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "Core.h"
-#include "spdlog/spdlog.h"
-#include "spdlog/fmt/ostr.h"
+
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#pragma warning(pop)
 
 namespace Ember {
 	class Log
@@ -10,16 +14,9 @@ namespace Ember {
 	public:
 		static void Init();
 
-		inline static Ref<spdlog::logger>& GetCoreLogger()
-		{
-			return s_CoreLogger;
-		}
+		inline static Ref<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
 
-		inline static Ref<spdlog::logger>& GetClientLogger()
-		{
-			return s_ClientLogger;
-		}
-
+		inline static Ref<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 	private:
 		static Ref<spdlog::logger> s_CoreLogger;
 		static Ref<spdlog::logger> s_ClientLogger;
@@ -31,11 +28,11 @@ namespace Ember {
 #define EB_CORE_INFO(...) ::Ember::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define EB_CORE_WARN(...) ::Ember::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define EB_CORE_ERROR(...) ::Ember::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define EB_CORE_FATAL(...) ::Ember::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define EB_CORE_FATAL(...) ::Ember::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
 #define EB_TRACE(...) ::Ember::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define EB_INFO(...) ::Ember::Log::GetClientLogger()->info(__VA_ARGS__)
 #define EB_WARN(...) ::Ember::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define EB_ERROR(...) ::Ember::Log::GetClientLogger()->error(__VA_ARGS__)
-#define EB_FATAL(...) ::Ember::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define EB_FATAL(...) ::Ember::Log::GetClientLogger()->critical(__VA_ARGS__)
